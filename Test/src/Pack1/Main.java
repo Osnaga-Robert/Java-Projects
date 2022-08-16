66dd8e841fe0ccae76c08bdb9d7407003cf3b6f3
Osnaga Robert <osnagarobert17@stud.acs.upb.ro>
2022-08-16 10:32:36 +0300

    I did it

--- Test/src/Pack1/Main.java
+++ Test/src/Pack1/Main.java
@@ -1,9 +1,10 @@
+package Pack1;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Ana are mere");
+		System.out.println("HAHA");
 	}
 
 }
